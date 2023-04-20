3df5301bc45dcfd10561c99c6b12bc5a50a3845b
Kickstartlab <info@kickstartlabs.io>
2023-04-20 19:34:07 +0500

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,17 +8,17 @@ export default function Footer() {
   return (
     <div className="font-poppins py-5 bg-yellow-50">
       <footer data-aos="zoom-in" className='container text-blue-200 mx-auto lg:px-20 font-russo px-5'>
-        <div className="grid lg:grid-cols-4 grid-cols-1 items-center justify-between py-12">
+        <div className="grid lg:grid-cols-4 grid-cols-1 items-center justify-between py-12 gap-y-8">
 
-          <div className="flex flex-col gap-y-6 items-center justify-center col-span-2">
+          <div className="flex flex-col gap-y-6 items-center justify-center lg:col-span-2">
             <div className="space-y-4 align-middle">
 
-              <img src={logo} alt='' className='mb-2'></img>
+              <img src={logo} alt='' className='mb-3 flex mx-auto'></img>
               <a href='/' alt='krypto'>
                 <img src={logo_2} alt=''></img>
               </a>
 
-              <div className='flex gap-6 items-center'>
+              <div className='flex gap-6 items-center justify-center'>
                 <a href='https://twitter.com/kryptoinu_bsc'>
                   <div className='w-14 h-14 pt-2 rounded-full bg-white-100 text-center'>
                     <i className="fa fa-twitter  fa-2x text-blue-100 pt-2" aria-hidden="true"></i>
